d5a02b7ef414cbf6a880b916d3b106dd8eeaecff
MasonPelt <mason@masonpelt.com>
2014-05-27 22:43:15 -0500

    a

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -17,9 +17,9 @@
       "speed": 500,             // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
       "pager": false,           // Boolean: Show pager, true or false
-      "nav": false,             // Boolean: Show navigation, true or false
+      "nav": true,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
-      "pause": false,           // Boolean: Pause on hover, true or false
+      "pause": true,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
